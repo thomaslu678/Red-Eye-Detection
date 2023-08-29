7be57a42e281b5155101e3bcef5700d6c5fa9805
thomaslu678 <thomaslu678@gmail.com>
2023-08-28 18:23:59 -0600

    javadoc

--- red-eye-detection/src/RedEyeDetection.java
+++ red-eye-detection/src/RedEyeDetection.java
@@ -1,5 +1,14 @@
+/**
+ * Detects a Red Eye from an algorithm as outlined in Pseudocode.txt
+ */
 public class RedEyeDetection {
 
+    /**
+     * Gets the middle pixel of any pixelated circle
+     * @param array a frame of pixels that encompasses the circle
+     * @param colorOptions strictly required colors of the circle
+     * @return the middle pixel
+     */
     public String getMiddlePixel(String[][] array, String[] colorOptions){
 
         int colorCount = 0;
@@ -36,6 +45,13 @@ public class RedEyeDetection {
 
     }
 
+    /**
+     * Checks if the middle pixel of two circles are aligned
+     * @param array a frame of pixels that encompasses both circles
+     * @param innerCircleOptions the color options for the inner circle
+     * @param outerCircleOptions the color options for the outer circle
+     * @return true if aligned, false if not
+     */
     public boolean middlePixelsAreAligned(String[][] array, String[] innerCircleOptions, String[] outerCircleOptions){
         if (getMiddlePixel(array, innerCircleOptions) == (getMiddlePixel(array, outerCircleOptions))){
             return true;
